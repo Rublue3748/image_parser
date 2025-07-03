b393a2bb9822b6893420393152f45737b9380b1d
Russell MacMillan <rmacmillan11@gmail.com>
2025-07-03 12:52:04 -0400

    Added documentation to inflate

--- inflate.cpp
+++ inflate.cpp
@@ -8,13 +8,66 @@
 #include <utility>
 #include <vector>
 
+/**
+ * @brief Inflates a deflated block of data from the bitstream. Returns whether there is potentially more data to decode (if the block is not marked
+ as final)
+ *
+ * @param data The bitstream to read from. Bitstream is modified regardless of if the data was valid
+ * @param return_data Buffer to return the data in. Appends to the end of the buffer
+ * @return Returns whether there is more data available (block is not marked as final)
+ */
 static bool inflate_block(Bitstream &data, std::vector<uint8_t> &return_data);
+
+/**
+ * @brief Copies the uncompressed block of data from Bitstream to return_data
+ *
+ * @param data The bitstream to copy from
+ * @param return_data The buffer to return data in
+ */
 static void copy_uncompressed(Bitstream &data, std::vector<uint8_t> &return_data);
+
+/**
+ * @brief Generates the static Huffman Trees for the literal and distance codes. The codes are described in
+ * https://datatracker.ietf.org/doc/html/rfc1951#page-12
+ *
+ * @return Pair of <literal tree, distance tree>
+ */
 static std::pair<Huffman_Tree, Huffman_Tree> get_static_trees();
+
+/**
+ * @brief Generates the dynamic Huffman Trees for the literal and distance codes, given by the stream.
+ *
+ * @param stream Datastream that points to the start of the dynamic trees block
+ * @return Pair of <literal tree, distance tree>
+ */
 static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream);
+
+/**
+ * @brief Reads in a block of data given the provided Huffman Trees
+ *
+ * @param stream Datastream to read from
+ * @param return_data Buffer to append data to
+ * @param trees Pair of <literal tree, distance tree> to use to decode the data
+ */
 static void get_data_from_trees(Bitstream &stream, std::vector<uint8_t> &return_data, std::pair<Huffman_Tree, Huffman_Tree> trees);
+
+/**
+ * @brief Takes a literal value >256 and converts it into a length, according to https://datatracker.ietf.org/doc/html/rfc1951#page-12
+ *
+ * @param literal The literal value to translate
+ * @param stream The datastream to read from for extra bits
+ * @return uint32_t The value of the length
+ */
 static uint32_t get_length_from_literal(uint32_t literal, Bitstream &stream);
-static uint32_t get_offset_from_literal(uint32_t literal, Bitstream &stream);
+
+/**
+ * @brief Takes an offset value and converts it into the actual offset, according to https://datatracker.ietf.org/doc/html/rfc1951#page-12
+ *
+ * @param offset The offset value to translate
+ * @param stream The datastream to read from for extra bits
+ * @return uint32_t The (positive) value of the offset
+ */
+static uint32_t get_offset_from_literal(uint32_t offset, Bitstream &stream);
 
 std::vector<uint8_t> inflate_data(const std::vector<uint8_t> &compressed_data)
 {
@@ -28,6 +81,8 @@ std::vector<uint8_t> inflate_data(const std::vector<uint8_t> &compressed_data)
     uint64_t FCHECK = stream.pop_bits(5);
     uint64_t FDICT = stream.pop_bits(1);
     uint64_t FLEVEL = stream.pop_bits(2);
+
+    // We aren't doing anything with these values as of now
     (void)CINFO;
     (void)FCHECK;
     (void)FLEVEL;
@@ -37,20 +92,19 @@ std::vector<uint8_t> inflate_data(const std::vector<uint8_t> &compressed_data)
         throw std::runtime_error("Unknown compression method!");
     }
 
-    // Skip dictonary CRC bit
+    // Skip dictonary CRC byte
     if (FDICT)
     {
         stream.pop_bits(8);
     }
-    // Decode inflate header
 
+    // Decode inflate blocks until we reach the final block
     while (inflate_block(stream, return_data))
         ;
 
     return return_data;
 };
 
-// Returns if there are more blocks to decode
 static bool inflate_block(Bitstream &stream, std::vector<uint8_t> &return_data)
 {
     bool is_final = stream.pop_bit();
@@ -98,6 +152,10 @@ static void copy_uncompressed(Bitstream &stream, std::vector<uint8_t> &return_da
 
 static std::pair<Huffman_Tree, Huffman_Tree> get_static_trees()
 {
+    // See https://datatracker.ietf.org/doc/html/rfc1951#page-7 (section 3.2.2)
+    // and https://datatracker.ietf.org/doc/html/rfc1951#page-12 (section 3.2.6)
+
+    // Generates a mapping of code lengths to values
     std::map<uint32_t, uint32_t> literal_map;
     for (size_t i = 0; i < 144; i++)
     {
@@ -115,7 +173,11 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_static_trees()
     {
         literal_map.insert({i, 8});
     }
+
+    // Generates the actual tree using the map
     Huffman_Tree literal_tree = Huffman_Tree::generate_tree(literal_map);
+
+    // Generates a 1:1 mapping for the distance tree so we can just reuse the distance code for the dynamic case
     Huffman_Tree distance_tree;
     for (size_t i = 0; i < 32; i++)
     {
@@ -126,7 +188,7 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_static_trees()
 
 static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream)
 {
-
+    // See https://datatracker.ietf.org/doc/html/rfc1951#page-13 (section 3.2.7)
     uint16_t HLIT = stream.pop_bits(5) + 257;
     uint16_t HDIST = stream.pop_bits(5) + 1;
     uint16_t HCLEN = stream.pop_bits(4) + 4;
@@ -135,6 +197,7 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
 
     std::map<uint32_t, uint32_t> code_lengths;
 
+    // Map given code lengths to codes
     for (size_t i = 0; i < HCLEN; i++)
     {
         uint32_t code_length = stream.pop_bits(3);
@@ -144,12 +207,16 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
         }
     }
 
+    // Build the code length tree using this first set of code lengths
     Huffman_Tree initial_tree = Huffman_Tree::generate_tree(code_lengths);
+
+    // Decode code lengths for the literals and the distances
+    // (Distance codes can backreference to literal codes, so decode as one big block and then split)
     std::vector<uint32_t> literals;
     while (literals.size() < HLIT + HDIST)
     {
         uint32_t val = initial_tree.get_next_value(stream);
-        if (val == 16)
+        if (val == 16) // Repeat previous code 3-6 times (read in 2 extra bits)
         {
             uint32_t repeat_amount = 3 + stream.pop_bits(2);
             for (size_t i = 0; i < repeat_amount; i++)
@@ -157,7 +224,7 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
                 literals.push_back(literals.back());
             }
         }
-        else if (val == 17)
+        else if (val == 17) // Repeat a code of zero 3-10 times (read in 3 extra bits)
         {
             uint32_t repeat_amount = 3 + stream.pop_bits(3);
             for (size_t i = 0; i < repeat_amount; i++)
@@ -165,7 +232,7 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
                 literals.push_back(0);
             }
         }
-        else if (val == 18)
+        else if (val == 18) // Repeat a code of zero 11-138 times (read in 7 extra bits)
         {
             uint32_t repeat_amount = 11 + stream.pop_bits(7);
             for (size_t i = 0; i < repeat_amount; i++)
@@ -173,18 +240,19 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
                 literals.push_back(0);
             }
         }
-        else
+        else // Push literal into buffer
         {
             literals.push_back(val);
         }
     }
+
     if (literals.size() > HLIT + HDIST)
     {
         throw std::runtime_error("Literals size somehow larger than HLIT + HDIST!");
     }
 
+    // Generate Literal Tree using first part of the buffer
     std::map<uint32_t, uint32_t> lit_map;
-
     for (uint32_t i = 0; i < HLIT; i++)
     {
         if (literals[i] != 0)
@@ -194,6 +262,7 @@ static std::pair<Huffman_Tree, Huffman_Tree> get_dynamic_trees(Bitstream &stream
     }
     Huffman_Tree literal_tree = Huffman_Tree::generate_tree(lit_map);
 
+    // Generate Distance Tree using second part of the buffer
     lit_map.clear();
     for (uint32_t i = 0; i < HDIST; i++)
     {
@@ -241,10 +310,12 @@ static void get_data_from_trees(Bitstream &stream, std::vector<uint8_t> &return_
 
 static uint32_t get_length_from_literal(uint32_t literal, Bitstream &stream)
 {
+    // https://datatracker.ietf.org/doc/html/rfc1951#page-12 (Section 3.2.5 tables)
+
     uint32_t length;
     if (literal >= 257 && literal <= 264)
     {
-        length = (literal - 257 + 3); // 257-264 -> 3-10
+        length = (literal - 257 + 3);
     }
     else if (literal >= 265 && literal <= 268)
     {
@@ -275,6 +346,8 @@ static uint32_t get_length_from_literal(uint32_t literal, Bitstream &stream)
 
 static uint32_t get_offset_from_literal(uint32_t literal, Bitstream &stream)
 {
+    // https://datatracker.ietf.org/doc/html/rfc1951#page-12 (Section 3.2.5 tables)
+
     uint32_t offset;
     if (literal <= 3)
     {
